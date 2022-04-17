2f1b88093f2ed0244817e5fb2307bd98550ddb11
MSMikl <michalbl4@gmail.com>
2022-04-17 19:13:25 +0300

    Выполнены задания урока

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from ast import Pass
 import os
 
 import django
+from django.forms.models import model_to_dict
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
@@ -10,3 +12,5 @@ from datacenter.models import Passcard  # noqa: E402
 if __name__ == '__main__':
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print(model_to_dict(Passcard.objects.get(id=1)))
+    print(Passcard.objects.filter(is_active=True).count())
